30f624486de041bb0d612b93008f5a7daab10374
BanChangGo <joo02261004@gmail.com>
2024-04-18 19:36:31 -0700

    added tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    bodkdfj
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-xl">
+        Bonjour from the pages file
+    </div>
+    
+</template>
